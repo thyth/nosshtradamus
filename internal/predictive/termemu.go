e25ed6bcc82ffb9bef45d24d86df7bd5251a3e74
Daniel Selifonov <ds@thyth.com>
2019-05-27 20:34:20 -0700

    Work in progress: duplicating termemu.cc example
    This is a WIP conversion of the termemu.cc mosh example into a
    Golang io.ReadWriteCloser interposer. Once completed, it should be
    capable of interposing on a terminal session, sending coherent
    deltas to the client, and be able to generate patches that take
    raw terminals to the current state of the interposed terminal.
    
    This example does not make use of prediction, but is a stepping
    stone to that functionality.

--- internal/predictive/termemu.go
+++ internal/predictive/termemu.go
@@ -46,68 +46,140 @@ type Interposer struct {
 	upstream         io.ReadWriteCloser
 	coalesceInterval time.Duration
 
+	pending *bytes.Buffer
+
 	width, height int
 
 	controlMutex *sync.Mutex
 
-	framebuffers     []*terminal.Framebuffer   // two framebuffers: front/back -- store screen state
-	frontFramebuffer int                       // designate which framebuffer index is the front
-	display          *terminal.Display         // used to generate deltas between framebuffers
-	emulator         *terminal.Complete        // processor of terminal control sequences
-	predictor        *overlay.PredictionEngine // speculative/predictive engine
+	state     *terminal.Framebuffer     // current state of the client's terminal
+	display   *terminal.Display         // used to generate deltas between framebuffers
+	emulator  *terminal.Complete        // processor of terminal control sequences
+	predictor *overlay.PredictionEngine // speculative/predictive engine
+
+	opened, initialized bool
 }
+
+// Notes:
+// - The 'termemu.cc' example program utilizes the Mosh terminal emulator classes to coalesce multiple updates into 20
+//   millisecond deltas. It instantiates instances of Terminal::Complete, Terminal::Framebuffer, Terminal::Display,
+//   Parser::UserByte, and Parser::Resize.
+//   - There are only 3 functions: main, emulate_terminal, and tick.
+//   - main probes terminal capabilities, launches a child process shell, and invokes emulate_terminal in the parent.
+//     - Informs the child shell that the TERM is 'xterm-256color', and NCURSES_NO_UTF8_ACS=1 via environment.
+//     - The latter environment variable informs ncurses programs to use different (mosh compatible?) line drawing mode.
+//   - emulate_terminal retrieves the current terminal size (of STDIN), informs the child of the terminal size (via its
+//     fd), allocates a Terminal::Complete and Terminal::Framebuffer using that terminal size, allocates a
+//     Terminal::Display, outputs the display.open() string to STDOUT, and then begins a 3 clause select.
+//     - The first clause reads user input from STDIN, bytewise wraps the characters of that input with Parser::UserByte
+//       instances, feeds those to the Terminal::Complete instance via complete.act(...), then writes the accumulated
+//       string ("terminal_to_host") to the child process (so that the child can react to user input).
+//     - The second clause reads child output (via its fd) as a string, and feeds it as one chunk to complete.act(...)
+//       similar to how the Parser::Action (e.g. Parser::UserByte) instances are used. This ALSO generates a string
+//       "terminal_to_host" that is fed back to the child process(!). No output is sent to STDOUT in this clause!
+//       - This is presumably to support terminal control sequences that report state (e.g. mouse position?) back to the
+//         child process, and is probably empty a lot of the time.
+//     - The third clause is triggered by SIGWINCH signals. These designate size changes of the parent terminal. The
+//       Terminal::Complete instance is first notified via complete.act(Parser::Resize(cols, rows)), and then the same
+//       size change is forwarded to the child process.
+//     - This select runs subject to an optional timeout. Initially, this waits indefinitely for one of these 3 clauses
+//       to be triggered. If a "tick" is valid (i.e. a new terminal has been drawn), a timeout is set for 20 ms, and
+//       subsequent triggers of the clauses will be coalesced until that timeout expires (forcing another call to tick).
+//     - After one of the clauses runs, or the timeout is triggered, the Terminal::Complete instance is asked for the
+//       current framebuffer state with complete.get_fb() -- as there is no prior linkage between the previously created
+//       Terminal::Framebuffer instance, this new framebuffer must be freshly allocated?
+//     - At this point, tick() is called with the existing state Terminal::Framebuffer, the new framebuffer just created
+//       by calling complete.get_fb(), and the Terminal::Display instance. The 3 clause select loop then iterates again.
+//     - Once the loop is broken, a final call to display.new_frame(...) is called to capture any unsent update, with
+//       the output string sent to STDOUT, and then the string produced by display.close() is also sent to STDOUT as a
+//       terminal cleanup method.
+//   - tick prints a frame delta to STDOUT and sets the (old) state reference to the new_frame, provided the last frame
+//     was drawn at least 20 milliseconds ago. It also keeps track of whether the display has been initialized (which
+//     is fed into the delta update creation function), and sets this initialization to (statically) true after sending
+//     the first update. These updates wre written to STDOUT, and represents the only way that output fed from the child
+//     process can reach the parent terminal (via state accumulated within the Terminal::Complete emulator, and the
+//     framebuffer instances returned from it).
+//   - TODO This method of retrieving Terminal::Framebuffer instances from Terminal::Complete suggests callee memory
+//     TODO ownership. Need to double check to see if the current (v0.1.1) go-mosh implementation leaks memory.
 
 func Interpose(rwc io.ReadWriteCloser, coalesceInterval time.Duration, width, height int) *Interposer {
 	return &Interposer{
 		upstream:         rwc,
 		coalesceInterval: coalesceInterval,
 
+		pending: nil,
+
 		width:  width,
 		height: height,
 
 		controlMutex: &sync.Mutex{},
 
-		framebuffers: []*terminal.Framebuffer{
-			terminal.MakeFramebuffer(width, height),
-			terminal.MakeFramebuffer(width, height),
-		},
-		frontFramebuffer: 0,
-		display:          terminal.MakeDisplay(true),
-		emulator:         terminal.MakeComplete(width, height),
-		predictor:        overlay.MakePredictionEngine(),
+		state:     terminal.MakeFramebuffer(width, height),
+		display:   terminal.MakeDisplay(true),
+		emulator:  terminal.MakeComplete(width, height),
+		predictor: overlay.MakePredictionEngine(),
+
+		opened:      false,
+		initialized: false,
 	}
 }
 
 // Close the terminal.
 func (i *Interposer) Close() error {
+	if i.opened {
+		closeStr := []byte(i.display.Close())
+		if i.pending == nil {
+			i.pending = &bytes.Buffer{}
+		}
+		_, _ = io.Copy(i.pending, bytes.NewReader(closeStr))
+	}
 	return i.upstream.Close()
 }
 
 // Read printed output from the terminal.
 func (i *Interposer) Read(p []byte) (int, error) {
+	if i.pending != nil {
+		// have pending bytes from a previous write to complete
+		if n, err := i.pending.Read(p); err == io.EOF {
+			i.pending = nil
+			return n, nil
+		} else {
+			return n, err
+		}
+	}
+	if !i.opened {
+		// need to send Terminal::Display.open() output first
+		i.opened = true
+		open := []byte(i.display.Open())
+		n := copy(p, open)
+		if n < len(open) {
+			open = open[n:]
+			if i.pending == nil {
+				i.pending = &bytes.Buffer{}
+			}
+			_, _ = io.Copy(i.pending, bytes.NewReader(open))
+		}
+		return n, nil
+	}
+	// TODO act upon the output and write-back terminal reports generated by the emulator
+	// TODO check if coalescence is ready to emit new output
 	return i.upstream.Read(p) // TODO work in progress
 }
 
 // Write user input to the terminal.
 func (i *Interposer) Write(p []byte) (int, error) {
-	buffer := &bytes.Buffer{}
+	terminalToHost := &bytes.Buffer{}
 	for _, b := range p {
 		s := i.emulator.Act(parser.MakeUserByte(int(b)))
-		buffer.WriteString(s)
+		terminalToHost.WriteString(s)
 	}
-	// TODO work in progress
-	return i.upstream.Write(p)
+	return i.upstream.Write(terminalToHost.Bytes())
 }
 
 // Change the width and height of the interposed terminal, in response to e.g. SIGWINCH or equivalent signal.
 func (i *Interposer) Resize(w, h int) {
 	i.controlMutex.Lock()
 	defer i.controlMutex.Unlock()
-	i.width = w
-	i.height = h
-	for _, framebuffer := range i.framebuffers {
-		framebuffer.Resize(w, h)
-	}
 	i.emulator.Act(parser.MakeResize(int64(w), int64(h)))
 }
 
@@ -116,7 +188,7 @@ func (i *Interposer) Resize(w, h int) {
 func (i *Interposer) CurrentContents() string {
 	i.controlMutex.Lock()
 	defer i.controlMutex.Unlock()
-	fb := i.framebuffers[i.frontFramebuffer]
+	fb := i.emulator.GetFramebuffer()
 	blank := terminal.MakeFramebuffer(i.width, i.height)
 	return i.display.NewFrame(false, blank, fb)
 }
