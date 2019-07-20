b8a29616a5b11b22b9f41d561225a5e6dd21ca6a
Daniel Selifonov <ds@thyth.com>
2019-07-19 23:40:34 -0700

    New ring delayer implementation
    Delays to the ring delayer are scheduled based on the time the write
    is received, rather than just injecting a sleep on the write call. This
    provides a closer approximation of network delay. However, the delay is
    only applied to writes. Reads are instantaneous. Should be sufficient
    for latency simulation tests, even if it isn't totally realistic.

--- internal/predictive/delay.go
+++ internal/predictive/delay.go
@@ -2,6 +2,7 @@ package predictive
 
 import (
 	"io"
+	"sync"
 	"time"
 )
 
@@ -14,7 +15,7 @@ type Delayer struct {
 func Delay(rwc io.ReadWriteCloser, delay time.Duration) *Delayer {
 	return &Delayer{
 		upstream: rwc,
-		delay: delay,
+		delay:    delay,
 	}
 }
 
@@ -38,4 +39,115 @@ func (d *Delayer) Write(p []byte) (int, error) {
 
 func (d *Delayer) Close() error {
 	return d.upstream.Close()
+}
+
+type RingDelayer struct {
+	upstream io.ReadWriteCloser
+	delay    time.Duration
+
+	ring     [][]byte
+	sendTime []time.Time
+	head     int
+	tail     int
+
+	cond  *sync.Cond
+
+	termination error
+	notifyChan  chan interface{}
+}
+
+func RingDelay(rwc io.ReadWriteCloser, delay time.Duration, ringSize int) *RingDelayer {
+	rd := &RingDelayer{
+		upstream: rwc,
+		delay:    delay,
+
+		ring:     make([][]byte, ringSize),
+		sendTime: make([]time.Time, ringSize),
+		head:     0,
+		tail:     0,
+
+		cond: sync.NewCond(&sync.Mutex{}),
+
+		termination: nil,
+		notifyChan:  make(chan interface{}, ringSize),
+	}
+	go func(rd *RingDelayer) {
+		for range rd.notifyChan {
+			rd.cond.L.Lock()
+
+			now := time.Now()
+			headTime := rd.sendTime[rd.head]
+			wait := headTime.Sub(now)
+			buffer := rd.ring[rd.head]
+
+			if wait > 0 {
+				// have time to wait -- release mutex and sleep
+				rd.cond.L.Unlock()
+				time.Sleep(wait)
+				rd.cond.L.Lock()
+			}
+
+			rd.ring[rd.head] = nil
+			rd.head++
+			rd.head %= len(rd.ring)
+			rd.cond.Signal() // notify one waiting client (if any) that there is now room in the ring
+			rd.cond.L.Unlock()
+
+			_, err := rd.upstream.Write(buffer)
+			rd.cond.L.Lock()
+			if err != nil {
+				rd.termination = err
+				close(rd.notifyChan)
+			}
+			rd.cond.L.Unlock()
+		}
+	}(rd)
+	return rd
+}
+
+func (rd *RingDelayer) Close() error {
+	if rd.termination != nil {
+		return rd.termination
+	}
+	rd.termination = io.EOF
+	close(rd.notifyChan)
+	return rd.upstream.Close()
+}
+
+func (rd *RingDelayer) Read(p []byte) (int, error) {
+	// read is instant -- only writes are delayed for ring delay
+	return rd.upstream.Read(p)
+}
+
+func (rd *RingDelayer) Write(p []byte) (int, error) {
+	if rd.termination != nil {
+		return 0, rd.termination
+	}
+	now := time.Now()
+	sendTime := now.Add(rd.delay)
+	buffer := make([]byte, len(p))
+	copy(buffer, p)
+
+	rd.cond.L.Lock()
+	for rd.ring[rd.tail] != nil {
+		// wrapped around the ring; wait until there is space available (possible longer delay)
+		rd.cond.Wait()
+	}
+
+	rd.ring[rd.tail] = buffer
+	rd.sendTime[rd.tail] = sendTime
+	rd.tail++
+	rd.tail %= len(rd.ring)
+
+	rd.cond.L.Unlock()
+	if rd.termination != nil {
+		return 0, rd.termination
+	}
+	rd.notifyChan <- true
+	return len(p), rd.termination
+}
+
+func (rd *RingDelayer) Callback(cb func()) {
+	// for simulation/testing of associated events on the same timescale
+	time.AfterFunc(rd.delay, cb)
 }
